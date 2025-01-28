95ed968f89229c8972f7e58ef45b72e3a9a1c038
ishaksebsib <ishaksebsib@gmail.com>
2025-01-28 14:38:39 +0300

    [ENH]: add channels and client and server

---
+++ src/bin/server.rs
@@ -0,0 +1,66 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use bytes::Bytes;
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
+        println!("Accepted");
+
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db: Db) {
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                println!("Setting {} : {:?}", cmd.key().to_string(), &cmd.value());
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                println!("Getting {}", cmd.key().to_string());
+                if let Some(value) = db.get(cmd.key()) {
+                    println!("Found {:?}", value);
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    println!("Key {} Not Found", cmd.key().to_string());
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        // Write the response to the client
+        connection.write_frame(&response).await.unwrap();
+    }
+}
